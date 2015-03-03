58aed468535517413076d5d9a581a1e76cfd8309
sunxu <sunxu.name@gmail.com>
2015-03-03 22:54:51 +0800

    add config

---
+++ application/config/email.php
@@ -0,0 +1,8 @@
+<?php
+
+if (! defined ( 'BASEPATH' ))
+	exit ( 'No direct script access allowed' );
+$config ['email'] ['smtp'] = '';
+$config ['email'] ['email'] = '';
+$config ['email'] ['password'] = '';
+$config ['email'] ['sender'] = '';
